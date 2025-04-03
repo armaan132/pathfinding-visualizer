487bc564d0cabea48321581fcb35f7702bf6b2be
armaan132 <armaan3@illinois.edu>
2025-04-03 16:53:01 -0700

    Added grid, cell functionality, added start and end and started on BFS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        //First line - test
         System.out.println("Welcome to proj");
+        Grid grid = new Grid(10, 10);
+
+        //Sets 0, 0 to start, 9, 9 to goal, allows user input later integrations
+        grid.getCell(0, 0).setStart(true);
+        grid.getCell(9, 9).setGoal(true);
+
+        //Set some obstacles for now - later I want to create random obstacles using system clock
+        //I want to use try - catch construct to generate maze that works - error message No path possible!
+        grid.getCell(3, 3).setObstacle(true);
+        grid.getCell(3, 4).setObstacle(true);
+        grid.getCell(3, 5).setObstacle(true);
+        grid.getCell(4, 5).setObstacle(true);
+        grid.getCell(5, 5).setObstacle(true);
+        grid.getCell(6, 5).setObstacle(true);
+
+
+
+
+        //displays the grid initially
+        grid.printGrid();
     }
 }
