f49bef4a8247d602614c6268435cb10d2b1afcd1
armaan132 <armaansaxena2948@gmail.com>
2025-05-08 17:11:46 -0700

    Commit update

--- src/PathfindingAlgorithms.java
+++ src/PathfindingAlgorithms.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 
 
 public class PathfindingAlgorithms {
@@ -40,6 +41,12 @@ public class PathfindingAlgorithms {
 
         }
 
+        //handling of no possible path - exits function
+        if (!path.containsKey(goal)) {
+            System.out.println("No path found BFS");
+            return;
+        }
+
         //reconstruct original path after goal is reached
         List<Cell> bfsPath = reconstructPath(path, start, goal);
         for (int i = 0; i < bfsPath.size(); i++) {
@@ -52,7 +59,58 @@ public class PathfindingAlgorithms {
         grid.printGrid();
     }
 
-    //travels backwards and consructs the BFS "shortest" path from goal to cell
+    public static void dfs(Grid grid, Cell start, Cell goal) {
+        //dfs uses stack, map parent child relationship
+        Stack<Cell> frontier = new Stack<>();
+        Map<Cell, Cell> path = new HashMap<>();
+
+        //set conditions for starting
+        frontier.push(start);
+        start.setVisited(true);
+        path.put(start, null);
+
+        //dfs implementation
+        while(!frontier.isEmpty()) {
+            Cell current = frontier.pop();
+
+            //finds goal - code breaks
+            if (current == goal) {
+                break;
+            }
+
+            List<Cell> neighbors = grid.getNeighbors(current);
+            for (int i = 0; i < neighbors.size(); i++) {
+                if (!neighbors.get(i).isObstacle() && !neighbors.get(i).isVisited()) {
+                    neighbors.get(i).setVisited(true);
+                    frontier.push(neighbors.get(i));
+                    path.put(neighbors.get(i), current);
+                }
+            }
+        }
+
+        //handling of no possible path - exits function
+        if (!path.containsKey(goal)) {
+            System.out.println("No path found DFS");
+            return;
+        }
+
+        //reconstruct path when goal is found
+        List<Cell> dfsPath = reconstructPath(path, start, goal);
+        for (int i = 0; i < dfsPath.size(); i++) {
+            Cell cell = dfsPath.get(i);
+            if (!cell.isStart() && !cell.isGoal()) {
+                cell.setPath(true);
+            }
+        }
+        
+        grid.printGrid();
+    }
+
+    public static void aStar(Grid grid, Cell start, Cell goal) {
+        
+    }
+
+    //travels backwards and consructs the path from goal to cell
     public static List<Cell> reconstructPath(Map<Cell, Cell> path, Cell start, Cell goal) {
         List<Cell> shortestPath = new ArrayList<>();
         Cell currentcell = goal;
@@ -65,4 +123,5 @@ public class PathfindingAlgorithms {
 
         return shortestPath;
     } 
+
 }
